aecf8598611e5a66043553d8c4f082293f95ed67
David Lavieri <daviddlavier@gmail.com>
2016-11-20 18:07:25 -0400

    Update Notifications and rename its test file

--- src/components/MainHeader/Notifications.js
+++ src/components/MainHeader/Notifications.js
@@ -2,13 +2,65 @@
 // Use of this source code is governed by a MIT License
 // License that can be found in the LICENSE file.
 
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
 
 import {Dropdown, commonProps} from './Dropdown'
+import MyLink from './../../utils/MyLink'
+const icon = cn => {
+  if (typeof cn === 'string' && cn.length > 0) {
+    return <i className={cn} />
+  }
+
+  return null
+}
+
+const Notification = ({title, iconClass, href, onClick}) => {
+  return (
+    <li onClick={onClick}>
+      <MyLink href={href}>
+        {icon(iconClass)} {title}
+      </MyLink>
+    </li>
+  )
+}
+
+Notification.propTypes = {
+  id: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  title: PropTypes.string,
+  iconClass: PropTypes.string,
+  href: PropTypes.string,
+  onClick: PropTypes.func
+}
+
+export {Notification}
 
 class Notifications extends Component {
+  getNotifications () {
+    const {data, onClick} = this.props
+
+    return data.map((item, index) => {
+      return <Notification
+        key={item.id + index}
+        {...item}
+        onClick={() => onClick(item.id)} />
+    })
+  }
+
   getItems () {
-    return []
+    const {header, footer} = this.props
+
+    return [
+      <li className='header'>{header}</li>,
+      <li>
+        <ul className='menu'>
+          {this.getNotifications()}
+        </ul>
+      </li>,
+      <li className='footer'><a href='#'>{footer}</a></li>
+    ]
   }
 
   getClassNames () {
---
+++ src/components/MainHeader/__tests__/dropdown-notifications-test.js
@@ -0,0 +1,161 @@
+// Copyright 2016 David Lavieri.  All rights reserved.
+// Use of this source code is governed by a MIT License
+// License that can be found in the LICENSE file.
+
+jest.unmock('./../Notifications')
+jest.unmock('./../../../utils/MyLink')
+
+import React from 'react'
+import {shallow, mount} from 'enzyme'
+import sinon from 'sinon'
+
+import Notifications, {Notification} from './../Notifications'
+
+describe('Menu.Dropdown.Notifications', () => {
+  const commonCB = () => {}
+
+  it('should add classes', () => {
+    const wrapper = shallow(
+      <Notifications
+        onToggle={commonCB}
+        data={[]} />
+    )
+
+    const classes = [
+      'notifications-menu',
+      'fa-bell-o',
+      'label-warning'
+    ]
+
+    expect(
+      wrapper.instance().getClassNames()
+    ).toEqual(classes)
+  })
+
+  it('should add inner menu', () => {
+    const wrapper = shallow(
+      <Notifications
+        onToggle={commonCB}
+        data={[]} />
+    )
+
+    const items = wrapper.instance().getItems()
+
+    expect(
+      items.length
+    ).toEqual(3)
+
+    expect(
+      shallow(items[0]).hasClass('header')
+    ).toBeTruthy()
+
+    expect(
+      shallow(items[1]).find('ul').hasClass('menu')
+    ).toBeTruthy()
+
+    expect(
+      shallow(items[2]).hasClass('footer')
+    ).toBeTruthy()
+  })
+
+  it('should set header and footer text', () => {
+    const wrapper = shallow(
+      <Notifications
+        onToggle={commonCB}
+        data={[]}
+        header='You have 1 Notification'
+        footer='View all notifications' />
+    )
+
+    const items = wrapper.instance().getItems()
+
+    expect(
+      items.length
+    ).toEqual(3)
+
+    expect(
+      shallow(items[0]).find('.header').text('You have 1 Notification')
+    ).toBeTruthy()
+
+    expect(
+      shallow(items[2]).find('.footer').contains(<a href='#'>View all notifications</a>)
+    ).toBeTruthy()
+  })
+
+  it('should have messages of type Notification', () => {
+    const wrapper = shallow(
+      <Notifications
+        onToggle={commonCB}
+        data={[{}]} />
+    )
+
+    const messages = wrapper.instance().getNotifications()
+
+    expect(
+      messages.length
+    ).toEqual(1)
+
+    expect(
+      mount(messages[0]).type()
+    ).toEqual(Notification)
+  })
+})
+
+describe('Menu.Dropdown.Notifications.Notification', () => {
+  const commonProps = {
+    id: 1,
+    title: 'New Notification',
+    onClick: () => {}
+  }
+
+  it('should have link with href (#) on empty href', () => {
+    const wrapper = mount(
+      <Notification {...commonProps}
+         />
+    )
+
+    expect(
+      wrapper.find('MyLink').find('Link').prop('to')
+    ).toEqual('#')
+  })
+
+  it('should have link with href set on props', () => {
+    const wrapper = mount(
+      <Notification
+        {...commonProps}
+        href='http://example.com'
+         />
+    )
+
+    expect(
+      wrapper.find('MyLink').find('a').prop('href')
+    ).toEqual('http://example.com')
+  })
+
+  it('should add title', () => {
+    const wrapper = mount(
+      <Notification {...commonProps}
+         />
+    )
+
+    expect(
+      wrapper.find('Link').text()
+    ).toContain('New Notification')
+  })
+
+  it('should trigger onclick on li message click', () => {
+    const spy = sinon.spy()
+    const wrapper = shallow(
+      <Notification
+        {...commonProps}
+        onClick={spy}
+         />
+    )
+
+    wrapper.find('li').simulate('click')
+
+    expect(
+      spy.called
+    ).toBeTruthy()
+  })
+})
