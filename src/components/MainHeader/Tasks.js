c2a5148e49ab4feea137c447830bd3c81b4c9262
David Lavieri <daviddlavier@gmail.com>
2016-11-20 19:08:34 -0400

    Update Tasks Dropdown menu and its tests

--- src/components/MainHeader/Tasks.js
+++ src/components/MainHeader/Tasks.js
@@ -2,13 +2,81 @@
 // Use of this source code is governed by a MIT License
 // License that can be found in the LICENSE file.
 
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
+import classnames from 'classnames'
 
 import {Dropdown, commonProps} from './Dropdown'
+import MyLink from './../../utils/MyLink'
+
+const Task = ({title, progress, progressClass, complete, href, onClick}) => {
+  const width = `${progress}%`
+  const pcn = classnames('progress-bar', progressClass)
+
+  return (
+    <li onClick={onClick}>
+      <MyLink href={href}>
+        <h3>
+          {title}
+          <small className='pull-right'>{progress}%</small>
+        </h3>
+
+        <div className='progress xs'>
+          <div className={pcn}
+            style={{width}}
+            role='progressbar'
+            aria-valuenow={progress}
+            aria-valuemin='0'
+            aria-valuemax='100'>
+            <span className='sr-only'>{progress}% {complete}</span>
+          </div>
+        </div>
+      </MyLink>
+    </li>
+  )
+}
+
+const stringOrNumeric = PropTypes.oneOfType([
+  PropTypes.string,
+  PropTypes.number
+])
+
+Task.propTypes = {
+  id: stringOrNumeric,
+  title: PropTypes.string,
+  progress: stringOrNumeric,
+  progressClass: PropTypes.string,
+  href: PropTypes.string,
+  onClick: PropTypes.func,
+  complete: PropTypes.string
+}
+
+export {Task}
 
 class Tasks extends Component {
+  getTasks () {
+    const {data, onClick, complete} = this.props
+
+    return data.map((item, index) => {
+      return <Task
+        key={item.id + index}
+        {...item}
+        complete={complete}
+        onClick={() => onClick(item.id)} />
+    })
+  }
+
   getItems () {
-    return []
+    const {header, footer} = this.props
+
+    return [
+      <li className='header'>{header}</li>,
+      <li>
+        <ul className='menu'>
+          {this.getTasks()}
+        </ul>
+      </li>,
+      <li className='footer'><a href='#'>{footer}</a></li>
+    ]
   }
 
   getClassNames () {
@@ -30,6 +98,9 @@ class Tasks extends Component {
   }
 }
 
-Tasks.propTypes = commonProps
+Tasks.propTypes = {
+  ...commonProps,
+  complete: PropTypes.string
+}
 
 export default Tasks
---
+++ src/components/MainHeader/__tests__/dropdown-tasks-test.js
@@ -0,0 +1,197 @@
+// Copyright 2016 David Lavieri.  All rights reserved.
+// Use of this source code is governed by a MIT License
+// License that can be found in the LICENSE file.
+
+jest.unmock('./../Tasks')
+jest.unmock('./../../../utils/MyLink')
+
+import React from 'react'
+import {shallow, mount} from 'enzyme'
+import sinon from 'sinon'
+
+import Tasks, {Task} from './../Tasks'
+
+describe('Menu.Dropdown.Tasks', () => {
+  const commonCB = () => {}
+
+  it('should add classes', () => {
+    const wrapper = shallow(
+      <Tasks
+        onToggle={commonCB}
+        data={[]} />
+    )
+
+    const classes = [
+      'tasks-menu',
+      'fa-flag-o',
+      'label-danger'
+    ]
+
+    expect(
+      wrapper.instance().getClassNames()
+    ).toEqual(classes)
+  })
+
+  it('should add inner menu', () => {
+    const wrapper = shallow(
+      <Tasks
+        onToggle={commonCB}
+        data={[]} />
+    )
+
+    const items = wrapper.instance().getItems()
+
+    expect(
+      items.length
+    ).toEqual(3)
+
+    expect(
+      shallow(items[0]).hasClass('header')
+    ).toBeTruthy()
+
+    expect(
+      shallow(items[1]).find('ul').hasClass('menu')
+    ).toBeTruthy()
+
+    expect(
+      shallow(items[2]).hasClass('footer')
+    ).toBeTruthy()
+  })
+
+  it('should set header and footer text', () => {
+    const wrapper = shallow(
+      <Tasks
+        onToggle={commonCB}
+        data={[]}
+        header='You have 1 Task'
+        footer='View all tasks' />
+    )
+
+    const items = wrapper.instance().getItems()
+
+    expect(
+      items.length
+    ).toEqual(3)
+
+    expect(
+      shallow(items[0]).find('.header').text('You have 1 Task')
+    ).toBeTruthy()
+
+    expect(
+      shallow(items[2]).find('.footer').contains(<a href='#'>View all tasks</a>)
+    ).toBeTruthy()
+  })
+
+  it('should have messages of type Task', () => {
+    const wrapper = shallow(
+      <Tasks
+        onToggle={commonCB}
+        data={[{}]} />
+    )
+
+    const messages = wrapper.instance().getTasks()
+
+    expect(
+      messages.length
+    ).toEqual(1)
+
+    expect(
+      mount(messages[0]).type()
+    ).toEqual(Task)
+  })
+})
+
+describe('Menu.Dropdown.Tasks.Task', () => {
+  const commonProps = {
+    id: 1,
+    title: 'New Task',
+    progress: 20,
+    progressClass: 'progress-bar-aqua',
+    onClick: () => {}
+  }
+
+  it('should have link with href (#) on empty href', () => {
+    const wrapper = mount(
+      <Task {...commonProps}
+         />
+    )
+
+    expect(
+      wrapper.find('MyLink').find('Link').prop('to')
+    ).toEqual('#')
+  })
+
+  it('should have link with href set on props', () => {
+    const wrapper = mount(
+      <Task
+        {...commonProps}
+        href='http://example.com'
+         />
+    )
+
+    expect(
+      wrapper.find('MyLink').find('a').prop('href')
+    ).toEqual('http://example.com')
+  })
+
+  it('should add title', () => {
+    const wrapper = mount(
+      <Task {...commonProps}
+         />
+    )
+
+    expect(
+      wrapper.find('h3').text()
+    ).toContain('New Task')
+  })
+
+  it('should add progress class', () => {
+    const wrapper = mount(
+      <Task {...commonProps}
+         />
+    )
+
+    expect(
+      wrapper.find('.progress-bar').hasClass('progress-bar-aqua')
+    ).toBeTruthy()
+  })
+
+  it('should add progress to style', () => {
+    const wrapper = mount(
+      <Task {...commonProps}
+         />
+    )
+
+    expect(
+      wrapper.find('.progress-bar').prop('style').width
+    ).toEqual('20%')
+  })
+
+  it('should add progress and complete screen only', () => {
+    const wrapper = mount(
+      <Task {...commonProps}
+        complete='Completed'
+         />
+    )
+
+    expect(
+      wrapper.find('.sr-only').text()
+    ).toContain('20% Completed')
+  })
+
+  it('should trigger onclick on li message click', () => {
+    const spy = sinon.spy()
+    const wrapper = shallow(
+      <Task
+        {...commonProps}
+        onClick={spy}
+         />
+    )
+
+    wrapper.find('li').simulate('click')
+
+    expect(
+      spy.called
+    ).toBeTruthy()
+  })
+})
