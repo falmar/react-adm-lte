878f2960f389fe5cca8fb778db73e78c30719712
David Lavieri <daviddlavier@gmail.com>
2016-12-15 09:18:54 -0400

    Add SidebarMenu Base Component

--- src/components/MainSidebar/SidebarMenu.js
+++ src/components/MainSidebar/SidebarMenu.js
@@ -1,3 +1,19 @@
 // Copyright 2016 David Lavieri.  All rights reserved.
 // Use of this source code is governed by a MIT License
 // License that can be found in the LICENSE file.
+
+import React, {PropTypes} from 'react'
+
+const SidebarMenu = ({children}) => {
+  return (
+    <ul className='sidebar-menu'>
+      {children}
+    </ul>
+  )
+}
+
+SidebarMenu.propTypes = {
+  children: PropTypes.node
+}
+
+export {SidebarMenu}
--- src/components/MainSidebar/__tests__/sidebarmenu-test.js
+++ src/components/MainSidebar/__tests__/sidebarmenu-test.js
@@ -1,3 +1,20 @@
 // Copyright 2016 David Lavieri.  All rights reserved.
 // Use of this source code is governed by a MIT License
 // License that can be found in the LICENSE file.
+
+jest.unmock('./../SidebarMenu')
+
+import React from 'react'
+import {shallow} from 'enzyme'
+
+import {SidebarMenu} from './../SidebarMenu'
+
+describe('Sidebar.Menu', () => {
+  it('should have classnames', () => {
+    const wrapper = shallow(<SidebarMenu />)
+
+    expect(
+      wrapper.hasClass('sidebar-menu')
+    ).toBeTruthy()
+  })
+})
