6f2da06a4988b4f785cf8296105e6d671cb663ee
David Lavieri <daviddlavier@gmail.com>
2016-12-17 18:50:53 -0400

    Update SidebarItem component and tests

--- src/components/MainSidebar/SidebarMenu.js
+++ src/components/MainSidebar/SidebarMenu.js
@@ -5,15 +5,50 @@
 import React, {PropTypes} from 'react'
 import classnames from 'classnames'
 
+import Link from './../../utils/Link'
+
 // -------------------- ITEM
-const Item = ({isTreeview}) => {
+const Item = (props) => {
+  const {isTreeview, header, active} = props
+  const {name, iconClass} = props
+  const {href, onClick} = props
+  const {children} = props
+
+  if (header) {
+    return <li className='header'>{name}</li>
+  }
+
+  const label = isTreeview ? <span>{name}</span> : name
+  const hasChildren = !(!children)
+
   return (
-    <li className={classnames({treeview: isTreeview})} />
+    <li className={classnames({treeview: isTreeview, active})}>
+      <Link href={href} onClick={onClick}>
+        <i className={classnames(iconClass)} />
+        {label}
+        {hasChildren &&
+          <span className='pull-right-container'>
+            <i className='fa fa-angle-left pull-right' />
+          </span>}
+      </Link>
+      {children}
+    </li>
   )
 }
 
 Item.propTypes = {
-  isTreeview: PropTypes.bool
+  name: PropTypes.string.isRequired,
+  iconClass: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.array,
+    PropTypes.string
+  ]),
+  href: PropTypes.string,
+  onClick: PropTypes.func,
+  active: PropTypes.bool,
+  isTreeview: PropTypes.bool,
+  header: PropTypes.bool,
+  children: PropTypes.element
 }
 
 export {Item}
--- src/components/MainSidebar/__tests__/sidebarmenu-test.js
+++ src/components/MainSidebar/__tests__/sidebarmenu-test.js
@@ -43,4 +43,111 @@ describe('Sidebar.Item', () => {
       wrapper.hasClass('treeview')
     ).toBeTruthy()
   })
+
+  it('should have classname active if prop provided', () => {
+    const wrapper = shallow(<Item active />)
+
+    expect(
+      wrapper.hasClass('active')
+    ).toBeTruthy()
+  })
+
+  it('should not have classname active if prop not provided', () => {
+    const wrapper = shallow(<Item />)
+
+    expect(
+      wrapper.hasClass('active')
+    ).toBeFalsy()
+  })
+
+  it('should be header if prop provided', () => {
+    const wrapper = shallow(<Item header name='Main Section' />)
+
+    expect(
+      wrapper.hasClass('header')
+    ).toBeTruthy()
+
+    expect(
+      wrapper.text()
+    ).toContain('Main Section')
+  })
+
+  it('should not be header if prop not provided', () => {
+    const wrapper = shallow(<Item />)
+
+    expect(
+      wrapper.hasClass('header')
+    ).toBeFalsy()
+  })
+
+  it('should have label without span when isTreeview not provided', () => {
+    const wrapper = shallow(<Item name='Main Section' />)
+
+    expect(
+      wrapper.contains('Main Section')
+    ).toBeTruthy()
+  })
+
+  it('should have label with span when isTreeview provided', () => {
+    const wrapper = shallow(<Item isTreeview name='Main Section' />)
+
+    expect(
+      wrapper.contains(<span>Main Section</span>)
+    ).toBeTruthy()
+  })
+
+  it('should not have arrow when children not passed', () => {
+    const wrapper = shallow(<Item />)
+
+    expect(
+      wrapper.find('span.pull-right-container').length
+    ).toEqual(0)
+
+    expect(
+      wrapper.find('i.fa.fa-angle-left.pull-right').length
+    ).toEqual(0)
+  })
+
+  it('should have arrow when children passed', () => {
+    const wrapper = shallow(<Item>anything</Item>)
+
+    expect(
+      wrapper.find('span.pull-right-container').length
+    ).toEqual(1)
+
+    expect(
+      wrapper.find('i.fa.fa-angle-left.pull-right').length
+    ).toEqual(1)
+  })
+
+  it('should have children', () => {
+    const wrapper = shallow(
+      <Item>
+        <Menu />
+      </Item>
+    )
+
+    expect(
+      wrapper.contains(<Menu />)
+    ).toBeTruthy()
+  })
+
+  it('should pass onClick and call it when provided', () => {
+    const spy = jest.fn()
+    const wrapper = shallow(<Item onClick={spy} />)
+
+    wrapper.find('MyLink').simulate('click')
+
+    expect(
+      spy
+    ).toHaveBeenCalled()
+  })
+
+  it('should have link with external link', () => {
+    const wrapper = shallow(<Item href='http://example.com' />)
+
+    expect(
+      wrapper.find('MyLink').prop('href')
+    ).toEqual('http://example.com')
+  })
 })
