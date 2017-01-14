23c5ad3d76d981cbce8f461cd459b0ed031ecbe6
David Lavieri <daviddlavier@gmail.com>
2017-01-13 20:55:49 -0400

    Update Layout Component remove use of jsdom package
    Since jest v15+ provide jsdom in its environment
    it may be not necessary to use for now

--- src/components/__tests__/layout-test.js
+++ src/components/__tests__/layout-test.js
@@ -4,20 +4,6 @@
 
 jest.unmock('./../Layout')
 
-var jsdom = require('jsdom').jsdom
-
-global.document = jsdom('<!doctype html><html><body></body></html>')
-global.window = document.defaultView
-Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
-    global[property] = document.defaultView[property]
-  }
-})
-
-global.navigator = {
-  userAgent: 'node.js'
-}
-
 import React from 'react'
 import {mount} from 'enzyme'
 
