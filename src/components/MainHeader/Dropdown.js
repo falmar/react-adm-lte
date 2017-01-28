7be5d033d479394455f02441f37dd1969a842986
David Lavieri <daviddlavier@gmail.com>
2017-01-28 16:24:45 -0400

    Start rebuild of MainHeader dropdown component

--- src/components/MainHeader/Dropdown.js
+++ src/components/MainHeader/Dropdown.js
@@ -6,104 +6,15 @@ import React, {Component, PropTypes} from 'react'
 import classnames from 'classnames'
 
 class Dropdown extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {timeoutId: null}
-    this.toggle = this.toggle.bind(this)
-    this.focus = this.focus.bind(this)
-    this.blur = this.blur.bind(this)
-    this.keydown = this.keydown.bind(this)
-    this.close = this.close.bind(this)
-  }
-
-  componentWillUnmount () {
-    clearTimeout(this.state.timeoutId)
-
-    if (this.props.open) {
-      this.props.onToggle(false)
-    }
-  }
-
-  toggle (event) {
-    event.preventDefault()
-
-    if (!this.props.open) {
-      this.props.onToggle(true)
-      setTimeout(() => {
-        this.menu.focus()
-      })
-    } else {
-      this.props.onToggle(false)
-    }
-  }
-
-  focus (event) {
-    clearTimeout(this.state.timeoutId)
-
-    this.props.onFocus(event)
-  }
-
-  blur (event) {
-    this.setState({timeoutId: setTimeout(() => {
-      this.props.onToggle(false)
-    })})
-  }
-
-  keydown (event) {
-    if (event.keyCode === 27) {
-      event.preventDefault()
-
-      if (this.props.open) {
-        this.props.onToggle(false)
-      }
-    }
-  }
-
-  close () {
-    this.props.onToggle(false)
-  }
-
   render () {
-    const {cn, header, children, open} = this.props
-
     return (
-      <li className={classnames('dropdown', cn, {open})}>
-        <a
-          href='#'
-          className='dropdown-toggle'
-          onClick={this.toggle}
-          onFocus={this.focus}
-          onBlur={this.blur}>
-          {header}
-        </a>
-        <ul
-          style={{outline: 'none'}}
-          tabIndex={-1}
-          ref={dom => { this.menu = dom }}
-          className='dropdown-menu'
-          onFocusCapture={this.focus}
-          onBlurCapture={this.blur}
-          onKeyDown={this.keydown}>
-          {children}
-        </ul>
-      </li>
+      <li />
     )
   }
 }
 
 Dropdown.propTypes = {
-  open: PropTypes.bool,
-  cn: PropTypes.string,
-  header: PropTypes.node.isRequired,
-  children: PropTypes.node,
-  onToggle: PropTypes.func.isRequired,
-  onFocus: PropTypes.func
-}
-
-Dropdown.defaultProps = {
-  onToggle: () => {},
-  onFocus: () => {}
+  children: PropTypes.element
 }
 
 export {Dropdown}
