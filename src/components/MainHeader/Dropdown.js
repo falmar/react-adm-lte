7869b1485f056be7e1133fc4f16e1f7bb753a3ba
David Lavieri <daviddlavier@gmail.com>
2017-01-28 17:28:40 -0400

    Build composable NavbarMenu's Dropdown Component

--- src/components/MainHeader/Dropdown.js
+++ src/components/MainHeader/Dropdown.js
@@ -6,14 +6,50 @@ import React, {Component, PropTypes} from 'react'
 import classnames from 'classnames'
 
 class Dropdown extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {eventRef: null}
+  }
+
+  componentDidMount () {
+    const ref = event => {
+      const {onToggle, open} = this.props
+
+      if (
+        open &&
+        typeof onToggle === 'function' &&
+        this.dom !== event.target &&
+        !this.dom.contains(event.target)
+      ) {
+        onToggle()
+      }
+    }
+
+    this.setState({eventRef: ref})
+    document.addEventListener('click', ref)
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('click', this.state.eventRef)
+  }
+
   render () {
+    const {className, children, open} = this.props
+
     return (
-      <li />
+      <li
+        ref={dom => (this.dom = dom)}
+        className={classnames('dropdown', className, {open})}>
+        {children}
+      </li>
     )
   }
 }
 
 Dropdown.propTypes = {
+  onToggle: PropTypes.func.isRequired,
+  open: PropTypes.bool,
   children: PropTypes.element
 }
 
--- src/components/MainHeader/__tests__/dropdown-test.js
+++ src/components/MainHeader/__tests__/dropdown-test.js
@@ -1,6 +1,7 @@
 // Copyright 2016 David Lavieri.  All rights reserved.
 // Use of this source code is governed by a MIT License
 // License that can be found in the LICENSE file.
+/* global Event */
 
 jest.unmock('./../Dropdown')
 
@@ -10,5 +11,163 @@ import {shallow, mount} from 'enzyme'
 import {Dropdown} from './../Dropdown'
 
 describe('Menu.Dropdown', () => {
+  it('should have className dropdown by default', () => {
+    const wrapper = shallow(<Dropdown />)
 
+    expect(
+      wrapper.hasClass('dropdown')
+    ).toBeTruthy()
+  })
+
+  it('should have className passed as prop', () => {
+    const wrapper = shallow(<Dropdown className='messages' />)
+
+    expect(
+      wrapper.hasClass('messages')
+    ).toBeTruthy()
+  })
+
+  it('should not have className open by default', () => {
+    const wrapper = shallow(<Dropdown />)
+
+    expect(
+      wrapper.hasClass('open')
+    ).toBeFalsy()
+  })
+
+  it('should have className open by if prop provided', () => {
+    const wrapper = shallow(<Dropdown open />)
+
+    expect(
+      wrapper.hasClass('open')
+    ).toBeTruthy()
+  })
+
+  it('should not call onToggle func if is not open', () => {
+    const onToggle = jest.fn()
+    const wrapper = mount(<Dropdown onToggle={onToggle} />)
+
+    document.dispatchEvent(new Event('click'))
+
+    wrapper.unmount()
+
+    expect(
+      onToggle
+    ).not.toHaveBeenCalled()
+  })
+
+  it('should call onToggle func if is open', () => {
+    const onToggle = jest.fn()
+    const wrapper = mount(<Dropdown open onToggle={onToggle} />)
+
+    document.dispatchEvent(new Event('click'))
+
+    wrapper.unmount()
+
+    expect(
+      onToggle
+    ).toHaveBeenCalled()
+  })
+
+  it('should remove onToggle event from dom when is unmounted', () => {
+    const onToggle = jest.fn()
+    const wrapper = mount(<Dropdown onToggle={onToggle} />)
+
+    wrapper.unmount()
+
+    document.dispatchEvent(new Event('click'))
+
+    expect(
+      onToggle
+    ).not.toHaveBeenCalled()
+  })
+
+  it('should pass down children', () => {
+    const wrapper = shallow(
+      <Dropdown>
+        <div>I'm a children</div>
+      </Dropdown>
+    )
+
+    expect(
+      wrapper.contains(<div>I'm a children</div>)
+    ).toBeTruthy()
+  })
+
+  it('should call onToggle on click window.document', () => {
+    const spy = jest.fn()
+    const wrapper = mount(
+      <Dropdown open onToggle={spy} />
+    )
+
+    window.document.dispatchEvent(new Event('click'))
+
+    expect(
+      spy
+    ).toHaveBeenCalled()
+
+    wrapper.unmount()
+    spy.mockReset()
+    window.document.dispatchEvent(new Event('click'))
+
+    expect(
+      spy
+    ).not.toHaveBeenCalled()
+  })
+
+  it('should call onToggle on click when click ocurs outside component', () => {
+    const spy = jest.fn()
+    const div = document.createElement('div')
+    const wrapper = mount(
+      <Dropdown open onToggle={spy} />
+    )
+
+    document.body.appendChild(div)
+    div.dispatchEvent(new Event('click', {bubbles: true}))
+    wrapper.unmount()
+    document.body.removeChild(div)
+
+    expect(
+      spy
+    ).toHaveBeenCalled()
+  })
+
+  it('should not call onToggle on click the component itself', () => {
+    const spy = jest.fn()
+    const div = document.createElement('div')
+    const wrapper = mount(
+      <Dropdown open onToggle={spy} />
+    , {attachTo: div})
+    const inst = wrapper.instance()
+
+    document.body.appendChild(div)
+    inst.dom.dispatchEvent(new Event('click', {bubbles: true}))
+    wrapper.unmount()
+    document.body.removeChild(div)
+
+    expect(
+      spy
+    ).not.toHaveBeenCalled()
+  })
+
+  it('should not call onToggle on click the component\'s children', () => {
+    const spy = jest.fn()
+    const div = document.createElement('div')
+    const wrapper = mount(
+      <Dropdown open onToggle={spy}>
+        <span />
+      </Dropdown>
+    , {attachTo: div})
+    const inst = wrapper.instance()
+    const span = inst.dom.getElementsByTagName('span')[0]
+
+    document.body.appendChild(div)
+    span.dispatchEvent(new Event('click', {bubbles: true}))
+    wrapper.unmount()
+    document.body.removeChild(div)
+
+    expect(
+      spy
+    ).not.toHaveBeenCalled()
+  })
 })
