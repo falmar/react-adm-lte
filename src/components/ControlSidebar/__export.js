5889489c7b00c9fbbc8ea34baf9979064a45ad8a
David Lavieri <daviddlavier@gmail.com>
2017-02-27 20:12:12 -0400

    Remove license text from files

--- src/components/ControlSidebar/__export.js
+++ src/components/ControlSidebar/__export.js
@@ -1,7 +1,3 @@
-// Copyright 2016 David Lavieri.  All rights reserved.
-// Use of this source code is governed by a MIT License
-// License that can be found in the LICENSE file.
-
 import ControlSidebar from './ControlSidebar'
 import ControlSidebarBackground from './ControlSidebarBackground'
 import NavTab from './NavTab'
--- src/components/MainSidebar/__export.js
+++ src/components/MainSidebar/__export.js
@@ -1,7 +1,3 @@
-// Copyright 2016 David Lavieri.  All rights reserved.
-// Use of this source code is governed by a MIT License
-// License that can be found in the LICENSE file.
-
 import MainSidebar from './MainSidebar'
 import UserPanel from './UserPanel'
 import SearchForm from './SearchForm'
