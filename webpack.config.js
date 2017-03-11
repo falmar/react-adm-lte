5eb19d25552382d34a5babf1eea1e5b3d68cd8b5
David Lavieri <daviddlavier@gmail.com>
2017-03-11 14:13:53 -0400

    Update dependencies packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,9 @@
-var debug = process.env.NODE_ENV !== 'production'
-var webpack = require('webpack')
 var path = require('path')
 
-var suffix = debug ? '' : '.min'
-var filename = 'react-adm-lte' + suffix + '.js'
-
 module.exports = {
-  target: 'node',
-  devtool: debug
-        ? 'inline-sourcemap' : null,
-  entry: path.join(__dirname, 'src', 'index.js'),
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: [
-            'react', 'es2015', 'stage-0'
-          ],
-          plugins: [
-            'transform-react-constant-elements',
-            'transform-react-inline-elements'
-          ]
-        }
-      }, {
-        test: /\.css$/,
-        loader: 'style!css!'
-      }, {
-        test: /\.json$/,
-        loader: 'json-loader'
-      }
-    ]
-  },
+  entry: './src/index.js',
   output: {
-    // target
-    libraryTarget: 'commonjs',
-    // output path
-    path: path.join(__dirname, 'dist'),
-    // output file
-    filename: filename
-  },
-  plugins: debug
-        ? [
-          new webpack.DefinePlugin({
-            'process.env': {
-              'NODE_ENV': JSON.stringify('development')
-            }
-          })
-        ] : [
-          new webpack.optimize.DedupePlugin(),
-          new webpack.optimize.OccurenceOrderPlugin(),
-          new webpack.optimize.UglifyJsPlugin({
-            mangle: false,
-            sourcemap: false,
-            compress: {
-              warnings: false
-            }
-          }),
-          new webpack.DefinePlugin({
-            'process.env': {
-              'NODE_ENV': JSON.stringify('production')
-            }
-          })
-        ]
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'react-adm-lte.js'
+  }
 }
