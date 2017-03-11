806393e80f6f2dbb2a6ad098bdefa6d6f831206f
David Lavieri <daviddlavier@gmail.com>
2017-03-11 14:53:18 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,18 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'react-adm-lte.js'
+    filename: 'react-adm-lte.js',
+    library: 'ReactADMLTE',
+    libraryTarget: 'commonjs-module'
+  },
+  externals: {
+    'react': 'React'
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }]
   }
 }
