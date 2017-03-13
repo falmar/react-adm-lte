28da36e37ac7f982e21b956c4318f1a0f7fe6251
David Lavieri <daviddlavier@gmail.com>
2017-03-13 12:57:23 -0400

    Export ContentWrapper Components
    For: https://github.com/falmar/react-adm-lte/issues/5

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ export {default as MainHeader} from './components/MainHeader'
 // Main Sidebar
 export {default as MainSidebar} from './components/MainSidebar'
 
+// Content Wrapper
+export {default as ContentWrapper} from './components/ContentWrapper'
+
 // Main Footer
 export {default as MainFooter} from './components/MainFooter'
 
