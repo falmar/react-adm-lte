a985e6b97c20df7c7aa56446667b8b5bc836f3d0
David Lavieri <daviddlavier@gmail.com>
2017-03-13 12:59:43 -0400

    Fix import ContentWrapper test

--- src/components/ContentWrapper/__tests__/wrapper-test.js
+++ src/components/ContentWrapper/__tests__/wrapper-test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {shallow} from 'enzyme'
 
-import Wrapper from '../Wrapper'
+import Wrapper from '../ContentWrapper'
 
 describe('Content Wrapper', () => {
   it('should be a <div> element', () => {
