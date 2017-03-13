eb66e73bcdadef3cc6214faf4a94599b90f279fb
David Lavieri <daviddlavier@gmail.com>
2017-03-13 12:54:13 -0400

    Export MainFooter Component
    For: https://github.com/falmar/react-adm-lte/issues/6

--- src/index.js
+++ src/index.js
@@ -7,5 +7,8 @@ export {default as MainHeader} from './components/MainHeader'
 // Main Sidebar
 export {default as MainSidebar} from './components/MainSidebar'
 
+// Main Footer
+export {default as MainFooter} from './components/MainFooter'
+
 // Control Sidebar
 export {default as ControlSidebar} from './components/ControlSidebar'
