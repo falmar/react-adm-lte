8779b4280c7f67933b54a6c5dd6c529d44d6609f
David Lavieri <daviddlavier@gmail.com>
2017-04-16 15:13:10 -0400

    Export Box Component

---
+++ src/components/Box/index.js
@@ -0,0 +1,12 @@
+import Box from './Box'
+import Header from './Header'
+import Tools from './Tools'
+import Body from './Body'
+import Footer from './Footer'
+
+Box.Header = Header
+Box.Tools = Tools
+Box.Body = Body
+Box.Footer = Footer
+
+export default Box
